62ee0120f49274dc39d34be08e6bb74ac3b32b7a
Peter Bengtsson <mail@peterbe.com>
2023-05-07 18:12:29 -0400

    debug

--- app/components/carbonad.tsx
+++ app/components/carbonad.tsx
@@ -19,6 +19,8 @@ export function CarbonAd() {
     };
   }, []);
 
+  console.log({ "process.env.NODE_ENV": process.env.NODE_ENV });
+
   if (process.env.NODE_ENV === "development") {
     return null;
   }
