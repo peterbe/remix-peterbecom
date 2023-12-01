7f8bca9ce12aca5e9d734bb485c16b70e6f78130
Peter Bengtsson <mail@peterbe.com>
2023-12-01 15:17:04 -0500

    remove got and only use axios

--- app/lib/get-data.ts
+++ app/lib/get-data.ts
@@ -1,29 +1,7 @@
-// import got from "got";
 import axios from "axios";
 import axiosRetry from "axios-retry";
 
 import { API_BASE } from "./_constants";
-
-// // The way `got` does retries:
-// //
-// //   sleep = 1000 * Math.pow(2, retry - 1) + Math.random() * 100
-// //
-// // So, it means:
-// //
-// //   1. ~1000ms
-// //   2. ~2000ms
-// //   3. ~4000ms
-// //
-// // ...if the limit we set is 3.
-// // Our own timeout, in ./middleware/timeout.js defaults to 10 seconds.
-// // So there's no point in trying more attempts than 3 because it would
-// // just timeout on the 10s. (i.e. 1000 + 2000 + 4000 + 8000 > 10,000)
-// const retryConfiguration = {
-//   limit: 4,
-// };
-// const timeoutConfiguration = {
-//   request: 2000,
-// };
 
 const RETRIES = 3;
 const TIMEOUT = 1000;
@@ -45,12 +23,7 @@ export async function get<T>(
   const t0 = new Date();
   const response = await axios.get<T>(API_BASE + uri, {
     timeout,
-    // const response = await got<T>(API_BASE + uri, {
-    // responseType: "json",
-    // throwHttpErrors,
     maxRedirects: followRedirect ? 10 : 0,
-    // retry: retryConfiguration,
-    // timeout: timeoutConfiguration,
     validateStatus: function (status) {
       if (throwHttpErrors) return status >= 200 && status < 300; // default
       return true;
@@ -58,9 +31,5 @@ export async function get<T>(
   });
   const t1 = new Date();
   console.log(`Fetch ${uri} took ${t1.getTime() - t0.getTime()} ms`);
-
-  // if (response.retryCount) {
-  //   console.warn(`Fetch had to retry on ${uri} ${response.retryCount} times`);
-  // }
   return response;
 }
