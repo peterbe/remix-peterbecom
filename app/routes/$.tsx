435346a4bae6735af021c81ceec56f1a4e27a2c2
Peter Bengtsson <mail@peterbe.com>
2024-03-27 21:43:20 -0400

    Handle POST submissions

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,3 +1,3 @@
 import View from "./_index";
-export { ErrorBoundary, headers, links, loader, meta } from "./_index";
+export { action, ErrorBoundary, headers, links, loader, meta } from "./_index";
 export default View;
