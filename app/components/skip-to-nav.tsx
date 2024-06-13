fc144db48ffc406a151f425d61f0f5db8b2a2d39
Peter Bengtsson <mail@peterbe.com>
2024-06-13 12:37:58 -0400

    programmatically go to things in skip-to-nav

--- app/components/skip-to-nav.tsx
+++ app/components/skip-to-nav.tsx
@@ -1,14 +1,31 @@
 export function SkipToNav() {
+  function handler(event: React.MouseEvent<HTMLAnchorElement>) {
+    const id = event.currentTarget.href.split("#")[1];
+    if (id) {
+      const element = document.getElementById(id);
+      if (element) {
+        event.preventDefault();
+        element.scrollIntoView();
+        element.focus();
+      }
+    }
+  }
   return (
     <ul className="skip-to-nav">
       <li>
-        <a href="#main-nav">Skip to main navigation</a>
+        <a href="#main-nav" onClick={handler}>
+          Skip to main navigation
+        </a>
       </li>
       <li>
-        <a href="#main-content">Skip to main content</a>
+        <a href="#main-content" onClick={handler}>
+          Skip to main content
+        </a>
       </li>
       <li>
-        <a href="#main-search">Skip to search</a>
+        <a href="#main-search" onClick={handler}>
+          Skip to search
+        </a>
       </li>
     </ul>
   );
