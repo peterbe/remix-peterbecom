63c815778990862b2d52f4905077f2969674a711
Peter Bengtsson <mail@peterbe.com>
2024-06-13 12:28:56 -0400

    Add tests for skip-to-nav

---
+++ app/components/skip-to-nav.tsx
@@ -0,0 +1,15 @@
+export function SkipToNav() {
+  return (
+    <ul className="skip-to-nav">
+      <li>
+        <a href="#main-nav">Skip to main navigation</a>
+      </li>
+      <li>
+        <a href="#main-content">Skip to main content</a>
+      </li>
+      <li>
+        <a href="#main-search">Skip to search</a>
+      </li>
+    </ul>
+  );
+}
