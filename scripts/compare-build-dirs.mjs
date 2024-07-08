808259d457892ee5e12328f5fdecdddd8d77130e
Peter Bengtsson <mail@peterbe.com>
2024-07-08 16:13:25 -0400

    hacky

--- scripts/compare-build-dirs.mjs
+++ scripts/compare-build-dirs.mjs
@@ -1,5 +1,5 @@
 import { join, relative } from "path";
-import { readFileSync, readdirSync, statSync } from "fs";
+import { readdirSync, statSync } from "fs";
 
 const [dir1, dir2] = process.argv.slice(2);
 main(dir1, dir2);
@@ -12,7 +12,6 @@ function main(dir1, dir2) {
   const totalDifference = totalSize1 - totalSize2;
   console.log(`\n**Difference: ${formatFilSize(totalDifference)}**\n`);
   if (!totalDifference) {
-    // console.log("The directories have the same size.");
     return;
   }
   const tuples = getTuples(dir1, dir2);
@@ -20,15 +19,16 @@ function main(dir1, dir2) {
   if (notables.length) {
     console.log("Notable changes...");
   }
-  for (const { path, before, after } of notables.sort(
+  for (const { path, before, after, isNew } of notables.sort(
     (a, b) => b.before - a.before
   )) {
+    if (isNew) {
+    }
     const sizeDifference = after - before;
     console.log(
-      ` - \`${relative(dir1, path)}\`: ${formatFilSize(before)} -> ${formatFilSize(after)} (${formatFilSize(sizeDifference)})`
+      ` - \`${path}\`: ${formatFilSize(before)} -> ${formatFilSize(after)} (${formatFilSize(sizeDifference)}) (${isNew ? "new" : "changed"})`
     );
   }
-  // console.log(tuples);
 }
 
 function getSizes(dir) {
@@ -38,16 +38,31 @@ function getSizes(dir) {
     if (ent.isDirectory()) {
       Object.assign(sizes, getSizes(join(dir, ent.name)));
     } else {
+      if (/chunk-[A-Z0-9]{8}\./.test(ent.name)) {
+        continue;
+      }
       const { size } = statSync(join(dir, ent.name));
       sizes[normalizeName(join(dir, ent.name))] = size;
-      // totalSize += size;
     }
   }
   return sizes;
 }
 function getTuples(baseDir, otherDir) {
-  const baseSizes = getSizes(baseDir);
-  const otherSizes = getSizes(otherDir);
+  const baseSizes = Object.fromEntries(
+    Object.entries(getSizes(baseDir)).map(([path, size]) => [
+      relative(baseDir, path),
+      size,
+    ])
+  );
+  const otherSizes = Object.fromEntries(
+    Object.entries(getSizes(otherDir)).map(([path, size]) => [
+      relative(otherDir, path),
+      size,
+    ])
+  );
+  // const otherSizes = getSizes(otherDir);
+  // console.log(baseSizes);
+  // console.log(otherSizes);
   const tuples = [];
   for (const [path, before] of Object.entries(baseSizes)) {
     const after = otherSizes[path] || 0;
@@ -55,6 +70,7 @@ function getTuples(baseDir, otherDir) {
       path,
       before,
       after,
+      isNew: !(path in otherSizes),
     });
   }
 
@@ -66,13 +82,15 @@ function normalizeName(name) {
 }
 
 function formatFilSize(size) {
+  const negative = size < 0;
+  size = Math.abs(size);
   const units = ["B", "KB", "MB", "GB"];
   let unitIndex = 0;
   while (size > 1024 && unitIndex < units.length - 1) {
     size /= 1024;
     unitIndex++;
   }
-  return `${size.toFixed(2)} ${units[unitIndex]}`;
+  return `${negative ? "-" : ""}${size.toFixed(2)} ${units[unitIndex]}`;
 }
 
 function getTotalSize(dir) {
