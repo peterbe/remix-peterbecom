f0f0bc0842b0f6c81addf1dcb6e0d6fd957fc1a0
Peter Bengtsson <mail@peterbe.com>
2024-07-09 08:37:25 -0400

    Delete some unused exports

--- app/utils/gtags.client.ts
+++ app/utils/gtags.client.ts
@@ -26,25 +26,25 @@ export const pageview = (url: string, trackingId: string) => {
   });
 };
 
-/**
- * @example
- * https://developers.google.com/analytics/devguides/collection/gtagjs/events
- */
-export const event = ({
-  action,
-  category,
-  label,
-  value,
-}: Record<string, string>) => {
-  if (!window.gtag) {
-    console.warn(
-      "window.gtag is not defined. This could mean your google analytics script has not loaded on the page yet.",
-    );
-    return;
-  }
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
-};
+// /**
+//  * @example
+//  * https://developers.google.com/analytics/devguides/collection/gtagjs/events
+//  */
+// export const event = ({
+//   action,
+//   category,
+//   label,
+//   value,
+// }: Record<string, string>) => {
+//   if (!window.gtag) {
+//     console.warn(
+//       "window.gtag is not defined. This could mean your google analytics script has not loaded on the page yet.",
+//     );
+//     return;
+//   }
+//   window.gtag("event", action, {
+//     event_category: category,
+//     event_label: label,
+//     value: value,
+//   });
+// };
