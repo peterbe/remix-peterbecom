e90b6ed037e722b1afb84fb160c44578b451d173
Peter Bengtsson <mail@peterbe.com>
2024-07-12 10:59:36 -0400

    Lyrics pages

--- app/routes/plog.blogitem-040601-1.$.tsx
+++ app/routes/plog.blogitem-040601-1.$.tsx
@@ -23,10 +23,25 @@ interface ServerData {
   comments: Comments;
 }
 
+type LyricsResult = {
+  id: number;
+};
+type LyricsResults = LyricsResult[];
+interface ServerSearchData {
+  results: LyricsResults;
+  metadata: {
+    limit: number;
+    desperate: boolean;
+    total: number;
+    search: string;
+  };
+  // comments: Comments;
+}
+
 export async function loader({ params, request }: LoaderFunctionArgs) {
   // invariant(params.oid, `params.oid is required`);
 
-  // console.log("IN plog.$oid.tsx PARAMS:", params);
+  console.log("IN plog.$blogitem-040601.$.tsx PARAMS:", params);
   const { pathname } = new URL(request.url);
   if (pathname.endsWith("/")) {
     return redirect(pathname.slice(0, -1));
@@ -37,13 +52,22 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
 
   const dynamicPage = params["*"] || "";
 
+  let isSearch = false;
   let page = 1;
   let oid = "blogitem-040601-1";
-  for (const part of dynamicPage.split("/")) {
+  const parts = dynamicPage.split("/");
+  console.log({ parts });
+  let search = "";
+
+  for (const part of parts) {
     if (!part) {
       // Because in JS,
       // > "".split('/')
       // [ '' ]
+      continue;
+    }
+    if (part === "search") {
+      isSearch = true;
       continue;
     }
     if (/^p\d+$/.test(part)) {
@@ -53,9 +77,34 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
       }
       continue;
     }
+    search = part;
   }
 
   const sp = new URLSearchParams({ page: `${page}` });
+  if (isSearch) {
+    if (!search) {
+      return redirect("/plog/blogitem-040601-1", 308);
+    }
+    sp.append("q", search);
+    const fetchURL = `/api/v1/lyrics/search?${sp}`;
+    const response = await get<ServerSearchData>(fetchURL);
+    if (response.status === 404) {
+      throw new Response("Not Found (oid not found)", { status: 404 });
+    }
+    if (response.status != 200) {
+      console.warn(`UNEXPECTED STATUS (${response.status}) from ${fetchURL}`);
+      throw new Error(`${response.status} from ${fetchURL}`);
+    }
+    const { results, metadata } = response.data;
+
+    const cacheSeconds = 60 * 60 * 12;
+
+    return json(
+      { results, metadata, page },
+      { headers: cacheHeaders(cacheSeconds) }
+    );
+  }
+
   const fetchURL = `/api/v1/plog/${encodeURIComponent(oid)}?${sp}`;
 
   const response = await get<ServerData>(fetchURL);
@@ -72,7 +121,7 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
 
   return json(
     { post, comments, page },
-    { headers: cacheHeaders(cacheSeconds) },
+    { headers: cacheHeaders(cacheSeconds) }
   );
 }
 
@@ -108,8 +157,16 @@ export const meta: MetaFunction<typeof loader> = ({ data, location }) => {
 };
 
 export default function View() {
-  const { post, comments, page } = useLoaderData<typeof loader>();
-  return <Lyricspost post={post} comments={comments} page={page} />;
+  const loaderData = useLoaderData<typeof loader>();
+  if ("post" in loaderData && "comments" in loaderData) {
+    const { post, comments, page } = loaderData;
+    return <Lyricspost post={post} comments={comments} page={page} />;
+  } else if ("results" in loaderData && "metadata" in loaderData) {
+    const { results, metadata } = loaderData;
+    return <LyricsSearch results={results} metadata={metadata} />;
+  } else {
+    throw new Error("Unexpected loader data");
+  }
 }
 
 export function headers({ loaderHeaders }: { loaderHeaders: Headers }) {
